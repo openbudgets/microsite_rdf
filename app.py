c880c184181539e8f6f579bd9b0abf8fc3dbaac5
Piero Hernandez <jeanpierohm@gmail.com>
2016-10-18 10:21:05 +0200

    feat (backend, frontend): add flask app as a user interface to navigate the data

---
+++ app.py
@@ -0,0 +1,51 @@
+from flask import Flask, render_template
+from sparql_template_manager.sparql import STM
+
+app = Flask(__name__)
+app.debug = True
+
+
+def double_quote(string):
+    return '"{}"'.format(string)
+
+
+@app.route('/')
+def index():
+    city = double_quote('aragon')
+    resp = STM.get_data('sparql_template_manager/top-dimensions-of-city.rq', [city])
+    dimensions = STM.predicates_list(resp)
+    links = [STM.next_link(city, d) for d in dimensions]
+    return render_template('layouts/index.html',
+                           data=[
+                               {'uri': uri, 'next_link': next_link}
+                               for uri, next_link in zip(dimensions, links)
+                               ]
+                           )
+
+
+@app.route('/<city>/<path:dimension>')
+def specific_dimension(city, dimension):
+    city = double_quote(city)
+    resp = STM.get_data('sparql_template_manager/specific-dimensions.rq',
+                        [dimension, city, dimension, city])
+    dimensions = STM.predicates_list(resp)
+
+    # we got to the deepest level in this classification tree, let's show
+    # transactions (observations) right away!
+    if not dimensions:
+        print(dimensions)
+        return render_template('layouts/error.html',
+                               data={'error': 'Transactions data coming soon!'})
+
+    # we still have some more nodes to explore
+    links = [STM.next_link(city, double_quote(d)) for d in dimensions]
+    return render_template('layouts/index.html',
+                           data=[
+                               {'uri': uri, 'next_link': next_link}
+                               for uri, next_link in zip(dimensions, links)
+                               ]
+                           )
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
