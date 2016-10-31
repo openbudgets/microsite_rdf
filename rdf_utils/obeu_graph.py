6dfa1af62fcc11dea24d22dd5eb6372f04828309
Piero Hernandez <jeanpierohm@gmail.com>
2016-10-31 16:00:32 +0100

    feat: add a different way to build the graph: abstract classes point to concrete classes and the former point to observations with their respective amount

--- rdf_utils/obeu_graph.py
+++ rdf_utils/obeu_graph.py
@@ -3,12 +3,38 @@ from stm import STM
 
 
 class OBEUGraph(nx.DiGraph):
-    def add_obeu_edges(self, city, query_file):
+    def add_obeu_observation_edges(self, city, year, query_file):
+        """
+        Query the SPARQL Template Manager (STM) to add required edges to the
+        graph
+        Two types of edges will be added:
+            - Edge (u, v) := u is the abstract class of the concrete class v
+            - Edge (v, w) := v is a concrete classification of observation w
+
+        :param city: (string) city name
+        :param year: (string) stringified number representing the year to query
+        :param query_file: (string) name of the file containing the SPARQL query
+                           to be used
+        :return: None
+        """
+        amounts = {}
+        response = STM.get_data(query_file, [city, year])
+
+        for elem in response['results']['bindings']:
+            self.add_edge(elem['abstract_class']['value'],
+                          elem['concrete_class']['value'])
+            self.add_edge(elem['concrete_class']['value'],
+                          elem['observation']['value'])
+
+            self.node[elem['observation']['value']]['amount'] = \
+                float(elem['amount']['value'])
+
+    def add_obeu_dimension_edges(self, city, query_file):
         """
         Query the SPARQL Template Manager (STM) to add required edges to the
         graph
         :param city: (string) city name
-        :param query_file: (string) name of the file containng the SPARQL query
+        :param query_file: (string) name of the file containing the SPARQL query
                            to be used
         :return: None
         """
@@ -43,7 +69,10 @@ class OBEUGraph(nx.DiGraph):
 
 if __name__ == '__main__':
     g = OBEUGraph()
-    g.add_obeu_edges(city='bonn', query_file='queries/dimensions-of-city.rq')
+    g.add_obeu_observation_edges(
+        city='Aragon',
+        year='2016',
+        query_file='queries/all-about-observations-from-city-in-a-year.rq')
     # get graph roots because they're top level dimensions of this city
     top_dimensions = g.root_nodes()
     pass
